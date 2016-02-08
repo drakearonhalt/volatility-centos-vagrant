df155bc62f1f11dae9066b2307a89847aafc00ee
drakeaornhalt <drakearonhalt@gmail.com>
2016-02-07 23:29:11 -0500

    Initial commit.

---
+++ provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yum install -y epel-release vim-enhanced git gcc
+yum install -y python-pip python-crypto
+pip install distorm3
+
+git clone https://github.com/volatilityfoundation/volatility.git
+
+cd volatility
+python setup.py build
+python setup.py install
+
+cd ~
+git clone https://github.com/volatilityfoundation/profiles.git
